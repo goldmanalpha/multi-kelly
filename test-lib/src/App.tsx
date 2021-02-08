d0678a19ab55f089bf06b1e4e026d10a83905d62
Aaron Goldman <1030451+goldmanalpha@users.noreply.github.com>
2021-02-08 16:56:35 -0500

    added watch test lib as child

---
+++ test-lib/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { KellyUi } from 'multi-kelly';
+
+function App() {
+  return (
+    <div className="App">
+      <KellyUi />
+    </div>
+  );
+}
+
+export default App;
